db30870df490cae2ddac70dd972c1c2727c97d7f
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-10-15 11:59:13 +0000

    [fix]4-8

--- db/migrate/20221015111414_create_post_images.rb
+++ db/migrate/20221015111414_create_post_images.rb
@@ -1,10 +1,11 @@
-class CreatePostimages < ActiveRecord::Migration[6.1]
+class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
-    create_table :postimages do |t|
-      t.integer :id
+    create_table :post_images do |t|
+
       t.string :shop_name
       t.text :caption
       t.integer :user_id
+
       t.timestamps
     end
   end
