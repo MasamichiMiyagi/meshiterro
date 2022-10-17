bf792997c0940549ff1ab387ede58a5ab8904c1d
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-10-17 09:13:15 +0000

    [finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
 
   #post_commentsは、post_imagesに結びつくものの為、親子関係になる。
+  #単数形の「resource」は、いいね機能の場合は「1人のユーザーは1つの投稿に対して1回しかいいねできない」という仕様であるため、destroyをする際にもユーザーidと投稿(post_image)idが分かれば、どのいいねを削除すればいいのかが特定できる為、params[:id]を使う必要が無い
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 
